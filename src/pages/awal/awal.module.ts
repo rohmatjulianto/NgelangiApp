cbf58a4c447f0c8e9f29b6e82681a21d84430ebd
rohmatjulianto <juliantojuly@gmail.com>
2017-12-05 17:03:20 +0700

    versi 1.0

---
+++ src/pages/awal/awal.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { AwalPage } from './awal';
+
+@NgModule({
+  declarations: [
+    AwalPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(AwalPage),
+  ],
+})
+export class AwalPageModule {}
